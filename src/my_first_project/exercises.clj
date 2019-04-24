4e48add31b014c7c4597379d65430aaef95148cf
Andre Ramos <andrelopesramos@outlook.com>
2019-04-23 23:27:32 -0300

    Mais testes didáticos

--- src/my_first_project/exercises.clj
+++ src/my_first_project/exercises.clj
@@ -5,3 +5,8 @@
   "The sum of 7654 and 1234"
   [x y]
   (+ x y))
+
+(defn greeting 
+  ([] "Hello, World!")
+  ([x] (str "Hello, " x"!"))
+  ([x y] (str x ", " y "!")))
--- test/my_first_project/exercises_test.clj
+++ test/my_first_project/exercises_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
            [my-first-project.exercises :refer :all]))
 
-(deftest sum
+(deftest simple-sum
   (testing "The sum of 7654 1234"
     (is (= 8888 (exercise1 7654 1234)))))
+
+(deftest multi-arity-greeting-test
+  (testing "Greeting on multi-arity functions"
+    (is (= "Hello, World!" (greeting))
+    (is (= "Hello, Clojure!" (greeting "Clojure"))
+    (is (= "Good morning, Clojure!" (greeting "Good morning" "Clojure")))))))
